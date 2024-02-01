cf2e09ef5ae005745f7835ca220146e3ac49ddd8
bengo <171782+gobengo@users.noreply.github.com>
2024-01-31 23:22:47 -0800

    wip

--- worker-nodejs/test/test-with-w3up-filestorage.js
+++ worker-nodejs/test/test-with-w3up-filestorage.js
@@ -5,12 +5,14 @@ import { getAddressUrl } from '../src/web.js'
 import { FileStorageW3up } from '../src/filestorage-w3up.js'
 import { readPackageJson } from '../src/fs-nodejs.js'
 import * as w3up from '@web3-storage/w3up-client'
+import * as ed25519 from '@ucanto/principal/ed25519'
+import assert from 'node:assert'
 
 await test('can use w3form with w3up', async () => {
-  const w3 = await w3up.create()
-  const space = await w3.createSpace(`test-${new Date().toISOString()}`);
-  await space.save()
-  w3.setCurrentSpace(space.did())
+  assert.ok(process.env.W3_PRINCIPAL, 'process.env has W3_PRINCIPAL')
+  const principal = ed25519.parse(process.env.W3_PRINCIPAL)
+  const w3 = await w3up.create({ principal })
+  console.log('w3 spaces', w3.spaces())
   const files = await FileStorageW3up.create(w3)
   const w3form = W3FormWorker.create({ files })
   const server = serve({ ...w3form, port: 0 })
