d226e6217277d8c18ca1af2fdeecfa3f3b1be2de
bengo <171782+gobengo@users.noreply.github.com>
2024-01-31 22:49:59 -0800

    wip

--- worker-nodejs/test/test-with-w3up-filestorage.js
+++ worker-nodejs/test/test-with-w3up-filestorage.js
@@ -7,7 +7,11 @@ import { readPackageJson } from '../src/fs-nodejs.js'
 import * as w3up from '@web3-storage/w3up-client'
 
 await test('can use w3form with w3up', async () => {
-  const files = await FileStorageW3up.create(await w3up.create())
+  const w3 = await w3up.create()
+  const space = await w3.createSpace(`test-${new Date().toISOString()}`);
+  await space.save()
+  w3.setCurrentSpace(space.did())
+  const files = await FileStorageW3up.create(w3)
   const w3form = W3FormWorker.create({ files })
   const server = serve({ ...w3form, port: 0 })
   await new Promise((resolve) => server.addListener('listening', resolve))
